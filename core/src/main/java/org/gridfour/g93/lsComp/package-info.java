52b9c9845e97572fed464c9be001bedd624c4941
gwlucastrig <contact.tinfour@gmail.com>
2020-08-26 20:03:01 -0400

    Add method of optimal predictors

---
+++ core/src/main/java/org/gridfour/g93/lsComp/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes related to the decoder for the LS8 optimal predictor
+ */
+package org.gridfour.g93.lsComp;
---
+++ demo/src/main/java/org/gridfour/demo/lsComp/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides utilities for the LS8 encoder.  The decoder logic is stored
+ * in a separate package.
+ *
+ */
+package org.gridfour.demo.lsComp;
