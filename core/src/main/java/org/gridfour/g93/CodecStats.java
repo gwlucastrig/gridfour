bb0d6c5075ec473f8ce42eec51e45603e914b035
gwlucastrig <contact.tinfour@gmail.com>
2020-10-03 20:11:36 -0400

    Revise entropy statistic

--- core/src/main/java/org/gridfour/g93/CodecStats.java
+++ core/src/main/java/org/gridfour/g93/CodecStats.java
@@ -42,178 +42,193 @@ package org.gridfour.g93;
 /**
  * A simple container for collecting statistics when analyzing compressed data.
  * <p>
- * This statistics collector is suited to the standard compressors implemented in
+ * This statistics collector is suited to the standard compressors implemented
+ * in
  * G93, but may not be applicable to other compressors, particularly those
- * implemented by a third party.  This class is provided as a convenience
+ * implemented by a third party. This class is provided as a convenience
  * for developers who which to implement their own compressors.
  */
 public class CodecStats {
 
-  final PredictiveTransformType pcType;
-  long nTilesCounted;
-  long nBytesTotal;
-  long nSymbolsTotal;
-  long nBitsOverheadTotal;
-  long nObservedM32;
-  long[] mCount = new long[256];
+    final PredictiveTransformType pcType;
+    long nTilesCounted;
+    long nBytesTotal;
+    long nSymbolsTotal;
+    long nBitsOverheadTotal;
 
-  /**
-   * Construct a statistics object for the specified predictive-transform.
-   * @param pcType a valid predictive-transform type.
-   */
-  public CodecStats(PredictiveTransformType pcType) {
-    this.pcType = pcType;
-  }
+    long nM32Counted;
+    long sumLengthM32;
+    long sumObservedM32;
+    double sumEntropyM32;
 
-  /**
-   * Get a label for the predictive-transform.
-   * @return a valid string
-   */
-  public String getLabel() {
-    return pcType.name();
-  }
-
-  /**
-   * Add metadata about the tile to the counts
-   * @param nBytesForTile the number of byts for the compressed version of the
-   * tile.
-   * @param nSymbolsInTile the number of values in the tile, typically
-   * the number of cells in the grid.
-   * @param nBitsOverhead Any compressor-specific overhead that an
-   * implementation wishes to track.
-   */
-  public void addToCounts(int nBytesForTile, int nSymbolsInTile, int nBitsOverhead) {
-    nTilesCounted++;
-    nBytesTotal += nBytesForTile;
-    nSymbolsTotal += nSymbolsInTile;
-    nBitsOverheadTotal += nBitsOverhead;
-  }
-
-  /**
-   * Add counts for the M32 symbols derived from the predictive-transforms
-   * for the tile.
-   * @param nM32 the total number of symbols in the encoded data
-   * @param m32 the encoded data
-   */
-  public void addCountsForM32(int nM32, byte[] m32) {
-    int []observed = new int[256];
-    for (int i = 0; i < nM32; i++) {
-      int index = m32[i]&0xff;
-      mCount[index]++;
-      observed[index] = 1;
+    /**
+     * Construct a statistics object for the specified predictive-transform.
+     *
+     * @param pcType a valid predictive-transform type.
+     */
+    public CodecStats(PredictiveTransformType pcType) {
+        this.pcType = pcType;
     }
-    for(int i=0; i<256; i++){
-      nObservedM32+=observed[i];
-    }
-  }
 
-  private static final double log2 = Math.log(2.0);
+    /**
+     * Get a label for the predictive-transform.
+     *
+     * @return a valid string
+     */
+    public String getLabel() {
+        return pcType.name();
+    }
 
-  /**
-   * Get the entropy for the data. In information theory, the term
-   * "entropy" is essentially an indicator of the number of bits actually
-   * needed to encode data.
-   * @return a positive value
-   */
- public  double getEntropy() {
-    long total = 0;
-    for (int i = 0; i < 256; i++) {
-      total += mCount[i];
+    /**
+     * Add metadata about the tile to the counts
+     *
+     * @param nBytesForTile the number of byts for the compressed version of the
+     * tile.
+     * @param nSymbolsInTile the number of values in the tile, typically
+     * the number of cells in the grid.
+     * @param nBitsOverhead Any compressor-specific overhead that an
+     * implementation wishes to track.
+     */
+    public void addToCounts(int nBytesForTile, int nSymbolsInTile, int nBitsOverhead) {
+        nTilesCounted++;
+        nBytesTotal += nBytesForTile;
+        nSymbolsTotal += nSymbolsInTile;
+        nBitsOverheadTotal += nBitsOverhead;
     }
-    if (total == 0) {
-      return 0;
-    }
-    double d = (double) total;
-    double s = 0;
-    for (int i = 0; i < 256; i++) {
-      if (mCount[i] > 0) {
-        double p = mCount[i] / d;
-        s += p * Math.log(p) / log2;
-      }
-    }
-    return -s;
-  }
 
-  /**
-   * Clear any accumulated counts
-   */
- public void clear() {
-    nTilesCounted = 0;
-    nBytesTotal = 0;
-    nSymbolsTotal = 0;
-    nBitsOverheadTotal = 0;
-  }
+    private static final double log2 = Math.log(2.0);
 
-  /**
-   * Get the number of bits per symbol in the pre-compression encoding.
-   * @return zero or a positive number
-   */
-  public double getBitsPerSymbol() {
-    if (nSymbolsTotal == 0) {
-      return 0;
-    }
-    return 8.0 * (double) nBytesTotal / (double) nSymbolsTotal;
-  }
+    /**
+     * Add counts for the M32 symbols derived from the predictive-transforms
+     * for the tile.
+     *
+     * @param nM32 the total number of symbols in the encoded data
+     * @param m32 the encoded data
+     */
+    public void addCountsForM32(int nM32, byte[] m32) {
+        if (nM32 <= 0) {
+            return;
+        }
+        this.nM32Counted++;
+        this.sumLengthM32 += nM32;
 
-  /**
-   * Get the number of tiles that have been counted.
-   * @return zero or a positive number
-   */
-  public long getTileCount() {
-    return nTilesCounted;
-  }
+        int[] observed = new int[256];
+        int[] mCount = new int[256];
+        for (int i = 0; i < nM32; i++) {
+            int index = m32[i] & 0xff;
+            mCount[index]++;
+            observed[index] = 1;
+        }
+        for (int i = 0; i < 256; i++) {
+            sumObservedM32 += observed[i];
+        }
 
-  /**
-   * Gets the average length of the M32 code length across tiles.
-   *
-   * @return a positive floating point value, potentially zero.
-   */
-  public double getAverageMCodeLength() {
-    long sum = 0;
-    for (int i = 0; i < mCount.length; i++) {
-      sum += mCount[i];
+        double d = (double) nM32;
+        double s = 0;
+        for (int i = 0; i < 256; i++) {
+            if (mCount[i] > 0) {
+                double p = mCount[i] / d;
+                s += p * Math.log(p) / log2;
+            }
+        }
+        this.sumEntropyM32 -= s;
     }
-    if (nTilesCounted > 0) {
-      return (double) sum / (double) nTilesCounted;
+
+    /**
+     * Get the entropy for the data. In information theory, the term
+     * "entropy" is essentially an indicator of the number of bits actually
+     * needed to encode data.
+     *
+     * @return a positive value
+     */
+    public double getEntropy() {
+        if (nM32Counted == 0) {
+            return 0;
+        }
+        return sumEntropyM32 / nM32Counted;
     }
-    return 0;
-  }
+
+    /**
+     * Clear any accumulated counts
+     */
+    public void clear() {
+        nTilesCounted = 0;
+        nBytesTotal = 0;
+        nSymbolsTotal = 0;
+        nBitsOverheadTotal = 0;
+    }
+
+    /**
+     * Get the number of bits per symbol in the pre-compression encoding.
+     *
+     * @return zero or a positive number
+     */
+    public double getBitsPerSymbol() {
+        if (nSymbolsTotal == 0) {
+            return 0;
+        }
+        return 8.0 * (double) nBytesTotal / (double) nSymbolsTotal;
+    }
+
+    /**
+     * Get the number of tiles that have been counted.
+     *
+     * @return zero or a positive number
+     */
+    public long getTileCount() {
+        return nTilesCounted;
+    }
+
+    /**
+     * Gets the average length of the M32 code length across tiles.
+     *
+     * @return a positive floating point value, potentially zero.
+     */
+    public double getAverageMCodeLength() {
+
+        if (nM32Counted == 0) {
+            return 0;
+        }
+        return (double) this.sumLengthM32 / (double) nM32Counted;
+
+    }
 
     /**
      * Gets the average number of unique M32 codes observed in
      * the compressed tile. The maximum number of unique M32 codes
      * is 256 (which would occur if all possible M32 codes were used).
      *
-   * @return a positive floating point value, potentially zero.
-   */
-  public double getAverageObservedMCodes() {
-    if (nTilesCounted > 0) {
-      return (double) nObservedM32 / (double) nTilesCounted;
+     * @return a positive floating point value, potentially zero.
+     */
+    public double getAverageObservedMCodes() {
+        if (nTilesCounted > 0) {
+            return (double) sumObservedM32 / (double) nTilesCounted;
+        }
+        return 0;
     }
-    return 0;
-  }
 
-
-  /**
-   * Get the average overhead per tile. Not all compressors tabulate
-   * this value.
-   * @return zero or a positive value.
-   */
-  public double getAverageOverhead() {
-    if (nTilesCounted == 0) {
-      return 0;
+    /**
+     * Get the average overhead per tile. Not all compressors tabulate
+     * this value.
+     *
+     * @return zero or a positive value.
+     */
+    public double getAverageOverhead() {
+        if (nTilesCounted == 0) {
+            return 0;
+        }
+        return (double) nBitsOverheadTotal / (double) nTilesCounted;
     }
-    return (double) nBitsOverheadTotal / (double) nTilesCounted;
-  }
 
-  /**
-   * Get the average length of encoded tiles, in bytes.
-   * @return zero or a positive number.
-   */
-  public double getAverageLength() {
-    if (nTilesCounted == 0) {
-      return 0;
+    /**
+     * Get the average length of encoded tiles, in bytes.
+     *
+     * @return zero or a positive number.
+     */
+    public double getAverageLength() {
+        if (nTilesCounted == 0) {
+            return 0;
+        }
+        return (double) nBytesTotal / (double) nTilesCounted;
     }
-    return (double) nBytesTotal / (double) nTilesCounted;
-  }
 }
