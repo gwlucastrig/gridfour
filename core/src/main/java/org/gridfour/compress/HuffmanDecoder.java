505c0c995cd8f1c5d0c73c59d3e982fdcf1762af
gwlucastrig <contact.tinfour@gmail.com>
2022-06-28 19:20:26 -0400

    Issue 24: Expedite Huffman decoder using alternate tree represention

--- core/src/main/java/org/gridfour/compress/HuffmanDecoder.java
+++ core/src/main/java/org/gridfour/compress/HuffmanDecoder.java
@@ -31,8 +31,17 @@
  * Date     Name         Description
  * ------   ---------    -------------------------------------------------
  * 10/2019  G. Lucas     Created
+ * 06/2022  G. Lucas     Refactored to use an integer-based index to
+ *                         represent the Huffman tree.
  *
  * Notes:
+ *
+ *  The reference implementation of the Huffman decoder implements the
+ * Huffman tree using a ordinary class for nodes.  That is a completely
+ * reasonable approach, but it is perplexingly slow.  So we refactored
+ * this class to represent the Huffman tree in an array of integers.
+ * It's a very old-school way of doing things and I am currently
+ * baffled as to why it should be necessary.
  *
  * -----------------------------------------------------------------------
  */
@@ -45,113 +54,122 @@ import org.gridfour.io.BitInputStore;
  */
 public class HuffmanDecoder {
 
-  private static class SymbolNode {
-
-    final boolean isLeaf;
-    final int symbol;
-
-    // Nodes for organizing the Huffman tree
-    SymbolNode left;
-    SymbolNode right;
-    SymbolNode parent;
-
-    SymbolNode() {
-      isLeaf = false;
-      symbol = -1;
-    }
-
-    SymbolNode(int symbol) {
-      this.isLeaf = true;
-      this.symbol = symbol;
-    }
-  }
-
   int nLeafNodes;
-  int nBranchNodes;
   int nBitsInTree;
 
-  public HuffmanDecoder() {
-
-  }
-
-  SymbolNode makeLeaf(int symbol) {
-    nLeafNodes++;
-    return new SymbolNode(symbol);
-  }
-
-  SymbolNode makeBranch() {
-    nBranchNodes++;
-    return new SymbolNode();
-  }
-
-  void clear() {
+  private void clear() {
     nLeafNodes = 0;
-    nBranchNodes = 0;
-    nBitsInTree=0;
+    nBitsInTree = 0;
   }
 
-  private SymbolNode decodeTree(BitInputStore store) {
+  private int[] decodeTree(BitInputStore store) {
+    int bit0 = store.getPosition();
     int nLeafsToDecode = store.getBits(8) + 1;
+    nLeafNodes = nLeafsToDecode;
     int rootBit = store.getBit();
     if (rootBit == 1) {
       // This is the special case where a non-zero root
       // bit indicates that there is only one symbol in the whole
       // encoding.   There is not a proper tree, only a single root node.
-      int symbol = store.getBits(8);
-      return makeLeaf(symbol);
+      int[] nodeIndex = new int[1];
+      nodeIndex[0] = store.getBits(8);
+      nBitsInTree = store.getPosition() - bit0;
+      return nodeIndex;
     }
 
-    SymbolNode root = makeBranch();
-    SymbolNode node = root;
+    // The array based representation of the Huffman tree
+    // is laid out as triplets of integer values each
+    // representing a node
+    //     [offset+0] symbol code (or -1 for a branch node)
+    //     [offset+1] index to left child node (zero if leaf)
+    //     [offset+2] index to right child node (zero if leaf)
+    // The maximum number of symbols is 256.  The number of nodes in
+    // a Huffman tree is always 2*n-1.  We allocate 3 integers per
+    // node, or 2*n*3.
+    int[] nodeIndex = new int[nLeafsToDecode * 6];
+    int nodeIndexCount;
+
+    // The maximum depth of a Huffman tree is
+    // the number of symbols minus 1. Since there are a maximum of 256
+    // symbols, we set the stack size to 256 to ensure that it will accomodate
+    // even the deepest tree. The probability of that actually happening
+    // is extremely small and such a tree would never be accepted as a
+    // compressed encoding by Gridfour because it would result in a
+    // encode text larger than its source.
+    //
+    // Initialization:
+    //    The root node is virtually placed on the base of the stack.
+    // It's left and right child-node references are zeroed out.
+    // The node type is set to -1 to indicate a branch node.
+    // The iStack variable is always set to the index of the element
+    // on top of the stack.
+    int[] stack = new int[nLeafsToDecode + 1];
+    int iStack = 0;
+    nodeIndex[0] = -1;
+    nodeIndex[1] = 0;
+    nodeIndex[2] = 0;
+    nodeIndexCount = 3;
+
     int nLeafsDecoded = 0;
     while (nLeafsDecoded < nLeafsToDecode) {
+      int offset = stack[iStack];
+      // We are going to generate a new node. It will be stored
+      // in the node-index array starting at position nodeIndexCount.
+      // We are going to store an integer reference to the new node as
+      // one of the child nodes of the node at the current position
+      // on the stack.  If the left-side node is already populates (offset+1),
+      // we will store the reference as the right-side child node (offset+2).
+      if (nodeIndex[offset + 1] == 0) {
+        nodeIndex[offset + 1] = nodeIndexCount;
+      } else {
+        nodeIndex[offset + 2] = nodeIndexCount;
+      }
+
       int bit = store.getBit();
       if (bit == 1) {
         // leaf node
-        int symbol = store.getBits(8);
-        SymbolNode leaf = makeLeaf(symbol);
         nLeafsDecoded++;
-        leaf.parent = node;
-        if (node.left == null) {
-          node.left = leaf;
-        } else {
-          node.right = leaf;
-          node = node.parent;
-          while (node != null && node.right != null) {
-            node = node.parent;
-          }
-          if (node == null) {
-            // we've just populated the right branch of the root node
-            // with a leaf.  We are done.
-            assert nLeafsDecoded == nLeafsToDecode : "Incomplete tree encoding";
-            break;
-          }
+        nodeIndex[nodeIndexCount++] = store.getBits(8);
+        nodeIndex[nodeIndexCount++] = 0; // not required, just a diagnostic aid
+        nodeIndex[nodeIndexCount++] = 0; // not required, just a diagnostic aid
+
+        if (nLeafsDecoded == nLeafsToDecode) {
+          // the tree will be fully populated, all nodes saturated.
+          // there will be no open indices left to populate.
+          // no further processing is required for the tree.
+          break;
         }
-      } else if (node.left == null) {
-        SymbolNode child = makeBranch();
-        child.parent = node;
-        node.left = child;
-        node = child;
+        // pop upwards on the stack until you find the first node with a
+        // non-populated right-side node reference. This may, in fact,
+        // be the current node on the stack.
+        while (nodeIndex[offset + 2] != 0) {
+          iStack--;
+          offset = stack[iStack];
+        }
       } else {
-        SymbolNode child = makeBranch();
-        child.parent = node;
-        node.right = child;
-        node = child;
+        // branch node, create a new branch node an push it on the stack
+        iStack++;
+        stack[iStack] = nodeIndexCount;
+        nodeIndex[nodeIndexCount++] = -1;
+        nodeIndex[nodeIndexCount++] = 0; // left node not populated
+        nodeIndex[nodeIndexCount++] = 0; // right node not populated
       }
     }
 
-    return root;
+    nBitsInTree = store.getPosition() - bit0;
+    return nodeIndex;
   }
 
   public boolean decode(BitInputStore input, int nSymbols, byte[] symbols) {
     clear();
 
-    int bit0 = input.getPosition();
-    SymbolNode root = decodeTree(input);
-    int bit1 = input.getPosition();
-    nBitsInTree = bit1-bit0;
-    if (root.isLeaf) {
-      byte symbol = (byte) (root.symbol);
+    // Decode the tree.  If the decoding detects a uniform encoding
+    // (all values are the same), just copy out the symbol at nodeIndex[0]
+    // and then return.
+    int[] nodeIndex = decodeTree(input);
+    if (nodeIndex.length == 1) {
+      // uniform encoding
+      byte symbol = (byte) nodeIndex[0];
       for (int i = 0; i < nSymbols; i++) {
         symbols[i] = symbol;
       }
@@ -159,20 +177,22 @@ public class HuffmanDecoder {
     }
 
     for (int i = 0; i < nSymbols; i++) {
-      SymbolNode node = root;
-      while (!node.isLeaf) {
-        if (input.getBit() == 0) {
-          node = node.left;
-        } else {
-          node = node.right;
-        }
+      int offset = nodeIndex[1 + input.getBit()]; // start from the root node
+      while (nodeIndex[offset] == -1) {
+        offset = nodeIndex[offset + 1 + input.getBit()];
       }
-      symbols[i] = (byte) node.symbol;
+      symbols[i] = (byte) nodeIndex[offset];
     }
     return true;
   }
 
-    public int getBitsInTreeCount() {
-        return this.nBitsInTree;
-    }
+  /**
+   * Gets the number of bits in the Huffman encoding tree for
+   * the most recently processed data set.
+   *
+   * @return a positive value, or zero if nothing was ever processed.
+   */
+  public int getBitsInTreeCount() {
+    return nBitsInTree;
+  }
 }
